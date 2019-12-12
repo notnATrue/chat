c19ae9a466c7f59eeafc8deaae0babba2072b028
Tony Trubchaninov <notnatrue@gmail.com>
2019-12-12 21:07:42 +0200

    update structure

---
+++ controllers/auth.js
@@ -0,0 +1,25 @@
+let db = require('../db');
+
+let createNewUser = async(currentUser) => {
+    console.log(currentUser)
+    const newUser = new db.user(currentUser);
+    newUser.save({newUser}, (err, data) => {
+     return console.log("created new user:" + data)
+    });
+  };
+  
+  function checkUserExistanse(currentUser) {
+    console.log(currentUser.username)
+    return new Promise(resolve => {
+      db.user.find({username : currentUser.username}, (err, data) => {
+        console.log(data)
+        resolve(data) 
+      });
+    });
+  };
+
+module.exports = {
+    createNewUser,
+    checkUserExistanse
+}
+ 
--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 require("dotenv").config();
+
 const db = require("./db");
-
 const asyn = require("./async");
-
+const auth = require('./controllers/auth');
+const verify = require('./controllers/verify');
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
 
 const app = express();
-
 const port = process.env.PORT || 3000;
-
-const bodyParser = require("body-parser");
-
-const cookieParser = require("cookie-parser");
-
-const jwt = require('jsonwebtoken');
-
-const { check, validationResult } = require('express-validator');
 
 app.use(cookieParser());
 app.use(bodyParser());
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static(`${__dirname}/src`));
 
 app.post('/signup', [
@@ -37,27 +31,22 @@ app.post('/signup', [
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   } else {
-    let result = await checkUserExistanse(req.body)
+    let result = await auth.checkUserExistanse(req.body)
     console.log("result = " + result)
     if (result.length === 0) {
-      createNewUser(req.body);
+      auth.createNewUser(req.body);
       return jwt.sign({user:req.body}, 'secretkey', (err, token) => {
         res.json({
           token
-        })
-      })
-
-      // res.send({
-      //   res: req.body
-      // })
-    }
-    else {
+        });
+      });
+    } else {
       return res.json({response : "user already exists"});
-    }
+    };
   };
 });
 
-app.post('/message', verifyToken, (req, res) => {
+app.post('/message', verify.verifyToken, (req, res) => {
   jwt.verify(req.token, 'secretkey', (err, authData) => {
     if (err) {
       res.sendStatus(403);
@@ -68,38 +57,9 @@ app.post('/message', verifyToken, (req, res) => {
       })
     }
   })
-})
+});
 
-let createNewUser = async(currentUser) => {
-  console.log(currentUser)
-  const newUser = new db.user(currentUser);
-  newUser.save({newUser}, (err, data) => {
-   return console.log("created new user:" + data)
-  });
-};
 
-function checkUserExistanse(currentUser) {
-  console.log(currentUser.username)
-  return new Promise(resolve => {
-    db.user.find({username : currentUser.username}, (err, data) => {
-      console.log(data)
-      resolve(data) 
-    });
-  });
-};
-
-function verifyToken(req, res, next) {
-  const bearerHeader = req.headers['authorization'];
-
-  if (typeof bearerHeader !== 'undefined') {
-    const bearer = bearerHeader.split(' ');
-    const bearerToken = bearer[1];
-    req.token = bearerToken;
-    next();
-  } else {
-    res.sendStatus(403)
-  }
-}
 app.get('/', (req, res) => {
   res.send('welcome to api');
 });
