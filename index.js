a53c4f17478c50a5cbd3f2b539580d3daac1acd0
Tony Trubchaninov <notnatrue@gmail.com>
2019-12-12 18:16:11 +0200

    update port listning

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const bodyParser = require("body-parser");
 
