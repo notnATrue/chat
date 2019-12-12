74e8b7039a54bc727d51741864f4c633fcbce686
Tony Trubchaninov <notnatrue@gmail.com>
2019-12-12 18:13:01 +0200

    add jwt check for message

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const bodyParser = require("body-parser");
 
 const cookieParser = require("cookie-parser");
 
+const jwt = require('jsonwebtoken');
+
 const { check, validationResult } = require('express-validator');
 
 app.use(cookieParser());
@@ -23,31 +25,50 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(`${__dirname}/src`));
 
-app.post('/signin', [
+app.post('/signup', [
 
   check('username').isEmail(),
 
   check('password').isLength({ min: 5 })
-],async (req, res) => {
+], async (req, res) => {
 
   const errors = validationResult(req);
   console.log(errors)
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   } else {
-  let result = await checkUserExistanse(req.body)
-        console.log("result = " + result)
-        if (result.length === 0) {
-          createNewUser(req.body);
-          return res.send({
-            res: req.body
-          })
-        }
-        else {
-          return res.json({response : "user already exists"});
-        }
+    let result = await checkUserExistanse(req.body)
+    console.log("result = " + result)
+    if (result.length === 0) {
+      createNewUser(req.body);
+      return jwt.sign({user:req.body}, 'secretkey', (err, token) => {
+        res.json({
+          token
+        })
+      })
+
+      // res.send({
+      //   res: req.body
+      // })
+    }
+    else {
+      return res.json({response : "user already exists"});
+    }
   };
 });
+
+app.post('/message', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+      message: "successfully",
+      authData
+      })
+    }
+  })
+})
 
 let createNewUser = async(currentUser) => {
   console.log(currentUser)
@@ -67,6 +88,18 @@ function checkUserExistanse(currentUser) {
   });
 };
 
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers['authorization'];
+
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403)
+  }
+}
 app.get('/', (req, res) => {
   res.send('welcome to api');
 });
