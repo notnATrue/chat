cf983ce25408997bc2b8b800e7b43dddf964676b
Tony Trubchaninov <notnatrue@gmail.com>
2019-12-12 21:20:58 +0200

    linter new architecture

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,24 @@
-let db = require('../db');
+const db = require("../db");
 
-let createNewUser = async(currentUser) => {
-    console.log(currentUser)
-    const newUser = new db.user(currentUser);
-    newUser.save({newUser}, (err, data) => {
-     return console.log("created new user:" + data)
+const createNewUser = async currentUser => {
+  console.log(currentUser);
+  const newUser = new db.user(currentUser);
+  newUser.save({ newUser }, (err, data) => {
+    return console.log(`created new user:${data}`);
+  });
+};
+
+function checkUserExistanse(currentUser) {
+  console.log(currentUser.username);
+  return new Promise(resolve => {
+    db.user.find({ username: currentUser.username }, (err, data) => {
+      console.log(data);
+      resolve(data);
     });
-  };
-  
-  function checkUserExistanse(currentUser) {
-    console.log(currentUser.username)
-    return new Promise(resolve => {
-      db.user.find({username : currentUser.username}, (err, data) => {
-        console.log(data)
-        resolve(data) 
-      });
-    });
-  };
+  });
+}
 
 module.exports = {
-    createNewUser,
-    checkUserExistanse
-}
- 
+  createNewUser,
+  checkUserExistanse
+};
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
-const db = require("./db");
-const asyn = require("./async");
-const auth = require('./controllers/auth');
-const verify = require('./controllers/verify');
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const jwt = require('jsonwebtoken');
-const { check, validationResult } = require('express-validator');
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
+const verify = require("./controllers/verify");
+const auth = require("./controllers/auth");
+const asyn = require("./async");
+const db = require("./db");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,51 +19,45 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/src`));
 
-app.post('/signup', [
-
-  check('username').isEmail(),
-
-  check('password').isLength({ min: 5 })
-], async (req, res) => {
-
-  const errors = validationResult(req);
-  console.log(errors)
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
-  } else {
-    let result = await auth.checkUserExistanse(req.body)
-    console.log("result = " + result)
+app.post(
+  "/signup",
+  [check("username").isEmail(), check("password").isLength({ min: 5 })],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    const result = await auth.checkUserExistanse(req.body);
+    console.log(`result = ${result}`);
     if (result.length === 0) {
       auth.createNewUser(req.body);
-      return jwt.sign({user:req.body}, 'secretkey', (err, token) => {
+      return jwt.sign({ user: req.body }, "secretkey", (err, token) => {
         res.json({
           token
         });
       });
-    } else {
-      return res.json({response : "user already exists"});
-    };
-  };
-});
+    }
+    return res.json({ response: "user already exists" });
+  }
+);
 
-app.post('/message', verify.verifyToken, (req, res) => {
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
+app.post("/message", verify.verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
     if (err) {
       res.sendStatus(403);
     } else {
       res.json({
-      message: "successfully",
-      authData
-      })
+        message: "successfully",
+        authData
+      });
     }
-  })
+  });
 });
 
-
-app.get('/', (req, res) => {
-  res.send('welcome to api');
+app.get("/", (req, res) => {
+  res.send("welcome to api");
 });
-
 
 // const session = require("./controllers/session");
 
